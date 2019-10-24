d9c358b29a1ab39ef48ce54af1093877f3fe839d
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2019-10-24 21:04:27 +0530

    database connection made.

---
+++ src/com/CODEX/DAO/DBConnection.java
@@ -0,0 +1,35 @@
+package com.CODEX.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	
+	public static Connection getConnect() 
+	{
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","cool") ;
+			return con;
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		return con;
+		
+	}
+	public static void main(String[] args) {
+
+		if(getConnect() != null)
+		{
+			System.out.println("Jay Jagannath ::::");
+		}
+
+	}
+
+}
